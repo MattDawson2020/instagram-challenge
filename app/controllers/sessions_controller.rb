ed4a55973b15fe3574a99b6e57fca8b10fe259a3
Matt <mattdawson18@gmail.com>
2021-05-22 17:22:57 +0100

    Login routes to basic user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to jams_path
-      flash[:notice] = "Welcome #{@user.name}"
+      redirect_to user_path(session[:user_id])
+      flash[:notice] = "Welcome #{@user.name}!"
     else
       redirect_to root_path
       flash[:error] = "Your login details were incorrect"
